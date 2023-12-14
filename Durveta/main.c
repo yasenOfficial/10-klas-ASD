2378dca577de531f6f7c349b76be3861bc62284b
Yasen Tsvetkov <yasentsv@gmail.com>
2023-12-14 22:28:06 +0200

    durveta pregovorr

--- Durveta/main.c
+++ Durveta/main.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <math.h>
 
+#include "queue.h"
+#define MAX_SIZE 100
+
+
 struct node_t {
     int value;
     struct node_t* left;
@@ -122,12 +126,6 @@ int minimal_height(struct node_t* root) {
     // Make more optimal at home
 }
 
-int balance(struct node_t* root) {
-    if (root == NULL) return 0;
-
-    // balance(root->left); AT HOME
-}
-
 void visualizeTree(struct node_t *root, int level)
 {
         if (root == NULL)
@@ -139,7 +137,88 @@ void visualizeTree(struct node_t *root, int level)
         visualizeTree(root->right, level + 1);
 }
 
+void print_bfs(struct node_t* root) {
+    if (root == NULL) return;
 
+    queue_t* q = init_queue();
+    queue_t* next = init_queue();
+
+    queue_push(q, root);
+
+    while (!queue_empty(next) || !queue_empty(q)) {
+        while (!queue_empty(q)) {
+            struct node_t* curr = queue_pop(q);
+            printf("%d ", curr->value);
+
+            if (curr->left != NULL)
+                queue_push(next, curr->left);
+            if (curr->right != NULL)
+                queue_push(next, curr->right);
+        }
+
+        printf("\n");
+        queue_t* tmp = q;
+        q = next;
+        next = tmp;
+    }
+}
+
+void print_dfs(struct node_t* root) {
+    if (root == NULL) return;
+
+    struct node_t* stack[MAX_SIZE];
+    int top = -1;
+
+    stack[++top] = root;
+
+    while (top >= 0) {
+        struct node_t* curr = stack[top--];
+        printf("%d ", curr->value);
+
+        if (curr->right != NULL) {
+            stack[++top] = curr->right;
+        }
+
+        if (curr->left != NULL) {
+            stack[++top] = curr->left;
+        }
+    }
+}
+
+void gather_nodes(struct node_t* root, struct node_t*** nodes, int* count) {
+    if (root->left != NULL)
+        gather_nodes(root->left, nodes, count);
+
+    *nodes = realloc(*nodes, sizeof(struct node_t*) * (*count + 1));
+    (*nodes)[*count] = root;
+    (*count)++;
+
+    if (root->right != NULL)
+        gather_nodes(root->right, nodes, count);
+}
+
+struct node_t* arrange_tree(struct node_t** nodes, int start, int end) {
+    if (start > end) {
+        return NULL;
+    }
+
+    int mid = (start + end) / 2;
+    struct node_t* root = nodes[mid];
+
+    root->left = arrange_tree(nodes, start, mid - 1);
+    root->right = arrange_tree(nodes, mid + 1, end);
+
+    return root;
+}
+
+struct node_t* balance_tree(struct node_t* root) {
+    struct node_t** nodes = NULL;
+    int count = 0;
+
+    gather_nodes(root, &nodes, &count);
+
+    return arrange_tree(nodes, 0, count - 1);
+}
 
 int main() {
     struct node_t* root = add(NULL, 4);
@@ -149,8 +228,25 @@ int main() {
     add(root, 3);
     add(root, 5);
     add(root, 7);
+    // add(root, 8);
+    // add(root, 9);
+
+    /*
+                  4
+                /   \
+               2     6
+             /   \ /   \
+            1    3 5    7
+    */
 
     visualizeTree(root, 0);
+
+    root = balance_tree(root);
+
+    visualizeTree(root, 0);
+
+    print_dfs(root);
+
 
     return 0;
 }
